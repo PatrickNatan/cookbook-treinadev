893e2fb2118a16e68e1e2acf99d9b5be238b39cc
Patrick Natan <patricknatan@hotmail.com>
2019-08-20 21:24:04 -0300

    Usuario logout

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -15,9 +15,27 @@ feature 'Admin register recipe type' do
       end
     #--A
     expect(current_path).to eq root_path
-    expect(page).to have_content("tst@email.com")
+    expect(page).to have_content(usr.email)
     expect(page).to have_link('Sair')
     expect(page).not_to have_link('Entrar')
     end
+
+    scenario 'log out' do
+      #A--
+      usr = User.create!(email:"tst@email.com", password:"123456")
+      #-A-
+      visit root_path
+      click_on 'Entrar'
+      within("form") do
+        fill_in 'Email', with: usr.email
+        fill_in 'Senha', with: '123456'
+        click_on 'Entrar'
+      end
+      click_on 'Sair'
+      #--A
+      expect(page).to have_link('Entrar')
+      expect(page).not_to have_link('Sair')
+      expect(page).not_to have_content(usr.email)
+ end
 
 end
