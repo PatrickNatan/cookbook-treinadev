e4fb54c6549e3725d1678458e652ddf34f9ca185
Patrick Natan <patricknatan@hotmail.com>
2019-08-26 21:55:26 -0300

    WIP admin aceita ou rejeita receitas

--- spec/features/admin_control_user_recipes_spec.rb
+++ spec/features/admin_control_user_recipes_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 feature 'admin control user recipes' do
     scenario "successfully" do
+        #A--
         recipe_type = RecipeType.create(name: 'Sobremesa')
         user = User.create!(email:"abc@email.com", password:"123456")
         recipe = Recipe.create!(title: 'pudim', difficulty: 'Médio',
@@ -13,14 +14,47 @@ feature 'admin control user recipes' do
         cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
         cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes',
         user: user,status: 1)
-        
+
         recipe.reload
         another_recipe.reload
-    
+        #-A-
         visit root_path
-    
+        #--A
         expect(page).to have_content('biscoito')
         expect(page).not_to have_content('pudim')
+    end
+    scenario "admin can accept or decline user recipes" do
+        #A--
+        admin = User.create!(email:"admin@email.com", password:"123456",admin: true)
+        recipe_type = RecipeType.create(name: 'Sobremesa')
+        user = User.create!(email:"abc@email.com", password:"123456")
+        recipe = Recipe.create!(title: 'pudim', difficulty: 'Médio',
+                        recipe_type: recipe_type, cuisine: 'Brasileira',
+                        cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                        cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes',
+                        user: user)
+        another_recipe = Recipe.create!(title: 'biscoito', difficulty: 'Médio',
+                        recipe_type: recipe_type, cuisine: 'Brasileira',
+                        cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                        cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes',
+                        user: user)
+        recipe.reload
+        another_recipe.reload
+        #-A-
+        visit root_path
+        click_on 'Entrar'
+        within("form") do
+              fill_in 'Email', with: user.email
+              fill_in 'Senha', with: '123456'
+              click_on 'Entrar'
+        end 
+        click_on 'Controlar receitas'
+        click_on "Aceitar #{recipe.title}"
+        click_on "Rejeitar #{another_recipe.title}"
+        visit root_path
+        #--A
+        expect(page).to have_content("#{recipe.title}")
+        expect(page).not_to have_content("#{another_recipe.title}")
     end
 end
 
